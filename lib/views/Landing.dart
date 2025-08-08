e05590ca4a60af102e59bc0aacf2f52848399a2a
yaseen <yzac1569@gmail.com>
2025-08-08 12:24:54 +0530

    added firebase

--- lib/views/Landing.dart
+++ lib/views/Landing.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/views/kp/Dashboard_kp.dart
+++ lib/views/kp/Dashboard_kp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DashboardKP extends StatelessWidget {
+  const DashboardKP({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
